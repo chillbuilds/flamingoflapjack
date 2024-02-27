0e6789aa5bf60406203a827df4027ece3c384c55
chillbuilds <will@chillbuilds.com>
2024-02-27 09:49:42 -0700

    updated ze reveal

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,6 +1,13 @@
 $(document).ready(function() {
     $("#password-input").focus()
 
+    $(document).keydown(function(event) {
+        if (event.shiftKey) {
+            $("#password-input").val('')
+            $("#password-input").focus()
+        }
+      })
+
     $("#password-input").on("input", function() {
         let inputValue = $(this).val()
         if(inputValue == 'asdf'){
