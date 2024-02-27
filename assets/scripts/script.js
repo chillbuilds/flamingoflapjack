85599f1ccab8df09cab4a90847d4b3a5baed65a5
chillbuilds <will@chillbuilds.com>
2024-02-27 10:21:47 -0700

    converted string to lower case for input check

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -10,7 +10,7 @@ $(document).ready(function() {
 
     $("#password-input").on("input", function() {
         let inputValue = $(this).val()
-        if(inputValue == 'asdf'){
+        if(inputValue.toLowerCase() == 'asdf'){
             console.log('got in')
             $('#err-fakeout').attr('style', 'display: none')
             $('.container').attr('style', 'display: inline-block')
