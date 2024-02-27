36df34590f9245bddf0134ce35fc24140af27fab
chillbuilds <will@chillbuilds.com>
2024-02-26 19:29:41 -0700

    updated file test src

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -4,6 +4,7 @@ $(document).ready(function() {
     $("#password-input").on("input", function() {
         let inputValue = $(this).val()
         if(inputValue == 'asdf'){
+            console.log('got in')
             $('.container').attr('style', 'display: inline-block')
         }
       })
