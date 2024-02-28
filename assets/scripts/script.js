8b77f3558e47022fd130ddfd2ca45162241accc2
chillbuilds <will@chillbuilds.com>
2024-02-27 18:20:01 -0700

    added esc action

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,20 +1,28 @@
+let unlocked = false
+
 $(document).ready(function() {
     $("#password-input").focus()
 
     $(document).keydown(function(event) {
-        if (event.shiftKey) {
+        if(event.shiftKey){
             $("#password-input").val('')
             $("#password-input").focus()
+        }
+        if(event.which === 27 && unlocked == true){
+            unlocked = false
+            $('#err-fakeout').attr('style', 'display: block')
+            $('.container').attr('style', 'display: none')
+            $('body').attr('style', 'background: white')
         }
       })
 
     $("#password-input").on("input", function() {
         let inputValue = $(this).val()
-        if(inputValue.toLowerCase() == 'asdf'){
-            console.log('got in')
+        if(inputValue.toLowerCase() == 'asdf' && unlocked == false){
             $('#err-fakeout').attr('style', 'display: none')
-            $('.container').attr('style', 'display: inline-block')
+            $('.container').attr('style', 'display: block')
             $('body').attr('style', 'background: url("https://flamingoflapjack.com/files/car-bg.gif") no-repeat center center fixed; background-size:cover; user-select:none;')
+            unlocked = true
         }
       })
 })
